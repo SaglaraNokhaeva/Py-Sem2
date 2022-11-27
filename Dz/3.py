985455de99e67d2324e37eb4ace0c47cd88d1447
Saglara <nohaevass@yandex.ru>
2022-11-28 00:07:23 +0300

    Update 3.py

--- Dz/3.py
+++ Dz/3.py
@@ -1,14 +1,14 @@
-# Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ 
+# Эту задачу взяла с видео семинара:
+# Задайте список из n чисел последовательности (1+\n)^n 
 # и выведите на экран их сумму.
-# Пример:
-# - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
 n = int(input("Введите натуральное число: "))
 posledovatelnost=[]
 summa=0
 for i in range(n):
-    x=(1+1/(i+1))**(i+1)
+    x=round((1+1/(i+1))**(i+1),2)
     posledovatelnost.append(x)
     summa+=x
-print(posledovatelnost)
-print(summa)
+print('Сумма = ',summa)
+for i in range(len(posledovatelnost)):
+     print(f"{i+1}: {posledovatelnost[i]}",end=",  ")
